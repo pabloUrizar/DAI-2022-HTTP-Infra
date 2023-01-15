54cee758caef5e4abd03607edb47d0963d700e3d
pabloUrizar <73521330+pabloUrizar@users.noreply.github.com>
2023-01-15 11:32:17 +0100

    Fini Step 5

--- step5/apache-php-image/content/index.php
+++ step5/apache-php-image/content/index.php
@@ -86,7 +86,7 @@
 
       <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="150">
         <div class="col-xl-6 col-lg-8">
-          <h1>Powerful Solutions With Gp<span>.</span></h1>
+          <h1>Lab 5 : HTTP Infrastructure</h1>
           <h2>We are team of talented digital marketers</h2>
 		  <?php
           echo '<h3 class="whiteTextOverride">';
