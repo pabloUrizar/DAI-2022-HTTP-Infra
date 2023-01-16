c50af7f339c02ab9da832a388319533250d1af2e
pabloUrizar <73521330+pabloUrizar@users.noreply.github.com>
2023-01-16 15:34:14 +0100

    Done

--- step6/apache-php-image/content/assets/js/animals.js
+++ step6/apache-php-image/content/assets/js/animals.js
@@ -1,9 +1,11 @@
 setInterval(async() => {
 
     const animals = await fetch('/api/').then(response => response.json());
+	
+	send = "Session ID : " + animals[0].hostname;
 
     if (animals.length > 0) {
-        send = "Ocean animals : " + animals[0].typeOfAnimal;
+        send += "  Ocean animals : " + animals[1].typeOfAnimal;
     }
 
     document.getElementById("api-animals").innerHTML = send}, 4000)
--- step6/express-image/src/index.js
+++ step6/express-image/src/index.js
@@ -4,6 +4,9 @@ var chance = new Chance();
 var express = require('express');
 var app = express();
 
+const os = require('os');
+
+
 app.get('/api', function(req, res) {
     res.send( generateAnimals() );
 });
@@ -14,16 +17,22 @@ app.listen(3000, function () {
 
 
 function generateAnimals() {
-    var numberOfAnimals = chance.integer({min: 0, max: 10});
-
+    var numberOfAnimals = chance.integer({min: 1, max: 10});
     console.log(numberOfAnimals);
+	
     var animals = [];
+	
+	animals.push({
+		hostname: os.hostname()
+	});
+		
     for (var i = 0; i < numberOfAnimals; i++) {
         var rnd = chance.integer({min: 0, max: 1});
         animals.push({
             typeOfAnimal: chance.animal({type: 'ocean'})
         });
     }
+	
     console.log(animals);
     return animals;
 }
